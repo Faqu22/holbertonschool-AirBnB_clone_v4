27f45a99813f1838e9639141bd1659f3ffd210ce
Faqu22 <facundoalvarez415@gmail.com>
2023-09-27 11:45:00 -0300

    Task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,13 @@
 window.onload = () => {
-    var check = $("input[type=checkbox]")
-    let list = []
-    check.each(function(index, element) {
-        check.click(function() {
-            if (element.checked) {
-                list[$(this).attr("data-id")] = ($(this).attr("data-name"))
-            }
-            else {
-                delete list[$(this).attr("data-id")]
-            }
-            $(" .amenities h4").text(Object.values(list).join(", "))
-        })
-        })
+    const list = {}
+    $("input:checkbox").click(function() {
+        if ($(this).is(":checked")) {
+            list[$(this).attr("data-id")] = $(this).attr("data-name")
+        }
+        else {
+            delete list[$(this).attr("data-id")]
+        }
+        console.log(list)
+        $(" .amenities h4").text(Object.values(list).join(", "))
+    })
 }
